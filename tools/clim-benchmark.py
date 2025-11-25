af7792dd56c521c9c9871c96bd8e2b04834b7614
Andreas Grafberger <18516896+andreas-grafberger@users.noreply.github.com>
2025-11-25 13:26:16 +0000

    add copied crps-stations and clim-benchmark scripts
    These scripts are copy-pasted from personal folders without modification
    (except for linting). The goal is to remove the tools directory and
    integrate these scripts properly into hyve.

---
+++ tools/clim-benchmark.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+
+import argparse
+import logging as log
+
+import dask
+import numpy as np
+import xarray as xr
+
+
+def percentile_ufunc(data, p_values, axis):
+    log.debug("Computing percentiles for array of size " + str(data.shape))
+
+    perc_list = []
+    for p in p_values:
+        if p == 0:
+            perc = np.amin(data, axis=axis)
+        elif p == 50:
+            perc = np.median(data, axis=axis)
+        elif p == 100:
+            perc = np.amax(data, axis=axis)
+        else:
+            perc = np.percentile(data, q=p, axis=axis)
+        perc_list.append(perc)
+
+    return perc_list
+
+
+def percentiles_xarray(data, p_values, dim, core_dim):
+    perc = xr.apply_ufunc(
+        percentile_ufunc,
+        data,
+        input_core_dims=[["time", *core_dim]],
+        dask="parallelized",
+        output_core_dims=[["percentiles", *core_dim]],
+        output_sizes={"percentiles": p_values.size},
+        output_dtypes=[data.dtype],
+        kwargs={"p_values": p_values, "axis": data.get_axis_num(dim)},
+    )
+    return perc
+
+
+def date_year(dates):
+    return dates.astype("datetime64[Y]").astype(int) + 1970
+
+
+def date_month(dates):
+    return dates.astype("datetime64[M]").astype(int) % 12 + 1
+
+
+def date_day(dates):
+    return (dates.astype("datetime64[D]") - dates.astype("datetime64[M]")).astype(
+        int
+    ) + 1
+
+
+def date_hour(dates):
+    return (dates.astype("datetime64[h]") - dates.astype("datetime64[D]")).astype(int)
+
+
+def date_ymd(date):
+    return date.astype("datetime64[D]")
+
+
+def clim_dates(dates, window_size):
+
+    index_1 = int(np.floor(window_size / 2.0))
+    index_2 = int(np.ceil(window_size / 2.0))
+
+    # remove leap days from array
+    leap_days = (
+        np.where(np.logical_and(date_month(dates) == 2, date_day(dates) == 29))[0] + 1
+    )
+    new_dates = np.delete(dates, leap_days)
+
+    # extend array at both ends (31 days window)
+    new_dates = np.concatenate((new_dates[-index_1:], new_dates, new_dates[:index_2]))
+
+    return new_dates
+
+
+def dates_range(dates, window, n_years, n_dates, day, ihour, freq, stride):
+
+    hour = 24.0 / freq * ihour
+    date_index = day * freq + ihour
+
+    index_range = np.arange(0, window, stride, dtype=int)[None, :]
+    # index_range[:, 2] = -1
+    index_range = index_range[index_range >= 0]
+    indexer = (
+        date_index
+        + index_range
+        + np.arange(0, n_years * n_dates, n_dates, dtype=int)[:, None]
+    ).flatten()
+    log.debug("{} ?= {}".format(indexer.size, (int(window / stride) + 1) * n_years))
+    assert (
+        indexer.size == (int(window / stride) + 1) * n_years
+    )  # MUST be +1 to include current day
+    dates_clim = dates[indexer]
+
+    hour_mask = np.where(date_hour(dates_clim) == hour)
+    dates_clim = dates_clim[hour_mask]
+
+    return dates_clim
+
+
+def coord_leap_year_dmh(freq):
+    if freq == 4:
+        dates = np.arange("2020-01-01T06", "2021-01-01T06", 6, dtype="datetime64[h]")
+    else:
+        dates = np.arange("2020-01-02T00", "2021-01-02T00", 24, dtype="datetime64[h]")
+
+    dates_str = np.datetime_as_string(dates, unit="h")
+
+    print(dates_str)
+    # days_months = np.char.replace(dates_str, '2020-', '')
+    days_months = [date[5:] for date in dates_str]
+
+    return days_months
+
+
+def compute_climatology(p_values, n_days, window, freq, stride, dates, dis):
+
+    # get dates stats
+    n_days_wo_leap = 365
+    n_years = int((dates.size - window) / (n_days_wo_leap * freq))
+    assert dates.size == n_days_wo_leap * n_years * freq + window
+
+    percentiles_list = []
+
+    log.info("Registering daily percentiles")
+    for day in range(n_days):
+
+        # replace leap days by February 28n_days_wo_leap
+        if day > 58:
+            day = day - 1
+
+        tmp_percentiles = []
+        for ihour in range(freq):
+            hour = 24.0 / freq * ihour
+
+            log.debug("day: {}, hour: {}".format(day, hour))
+
+            # Apply 31 days window around date
+            dates_clim = dates_range(
+                dates, window, n_years, n_days_wo_leap * freq, day, ihour, freq, stride
+            )
+            log.debug(dates_clim[: int(window / stride + 1)])
+            percentiles = dask.delayed(percentiles_xarray)(
+                dis.sel(time=dates_clim), p_values, "time", core_dim
+            )
+            percentiles = percentiles.rename({"percentiles": "ensemble"})
+            tmp_percentiles.append(percentiles)
+
+        tmp_percentiles = dask.compute(*tmp_percentiles)
+        percentiles_list.extend(tmp_percentiles)
+
+    log.info("Exporting climatology dataset")
+    new_coords = {"time": coord_leap_year_dmh(freq)}
+    new_coords.update({dim: dis.coords[dim] for dim in core_dim})
+    clim = xr.DataArray(
+        np.zeros((n_days * freq, p_values.size, dis[0].size)),
+        dims=("time", "ensemble", *core_dim),
+        coords=new_coords,
+    )
+    for idate in range(n_days * freq):
+        clim[idate] = percentiles_list[idate]
+    print(clim)
+    clim = clim.rename("dis")
+
+    return clim
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reanalysis", required=True, help="reanalysis dataset file")
+    parser.add_argument("--output", required=True, help="output climatology file")
+    parser.add_argument(
+        "--window", type=int, default=30, help="moving window for percentiles (in days)"
+    )
+    parser.add_argument(
+        "--timestep", type=int, default=6, help="reanalysis timestep (in hours)"
+    )
+    parser.add_argument(
+        "--n_days",
+        type=int,
+        default=366,
+        help="number of days to compute in climatology",
+    )
+    parser.add_argument(
+        "--stride",
+        type=int,
+        default=1,
+        help="Stride used to compute the climatology (7 for seasonal)",
+    )
+    parser.add_argument(
+        "--start_date",
+        default="1991-01-01T06:00:00",
+        help="start date of the climatology",
+    )
+    parser.add_argument(
+        "--end_date", default="2020-01-01T00:00:00", help="end date of the climatology"
+    )
+    parser.add_argument(
+        "--scheduler",
+        default="threads",
+        choices=["synchronous", "threads"],
+        help="reanalysis dataset file",
+    )
+    parser.add_argument(
+        "--log",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="set logging level",
+    )
+
+    args = parser.parse_args()
+
+    log.basicConfig(
+        level=args.log,
+        format="dummy-clim - (%(processName)-10s) %(levelname)s: %(message)s",
+    )
+
+    log.info("Computing a dummy climatology from reanalysis dataset")
+
+    log.info(" - dask: {}".format(args.scheduler))
+    log.info(" - reanalysis: {}".format(args.reanalysis))
+    log.info(" - output: {}".format(args.output))
+    log.info(" - window: {}".format(args.window))
+    log.info(" - timestep: {}".format(args.timestep))
+    log.info(" - stride: {}".format(args.stride))
+    log.info(" - n_days: {}".format(args.n_days))
+
+    freq = int(24 / args.timestep)
+    log.info("Computing climatology using {} steps per day".format(freq))
+
+    stride = args.stride
+    window = args.window
+    step_window = window * freq - (window * freq) % stride + 1
+
+    # considering statistics on a leap year
+    n_days_w_leap = 366
+    n_days = args.n_days
+
+    # percentiles
+    p_values = np.arange(0, 101, 10)
+
+    with dask.config.set(scheduler=args.scheduler):
+
+        # read reanalysis dataset
+        reanalysis_file = args.reanalysis
+        ds_reanalysis = xr.open_dataset(reanalysis_file, chunks={})
+
+        # extract core dims from dataset
+        core_dim = list(ds_reanalysis.dims)
+        core_dim.remove("time")
+        log.info("Core dimensions found in dataset: {}".format(core_dim))
+        dis = (
+            ds_reanalysis["dis"].sel(time=slice(args.start_date, args.end_date)).load()
+        )
+        log.info("discharge dataset {}".format(dis))
+
+        dates = clim_dates(dis.time.values, step_window)
+        dates_index = np.arange(dates.size, dtype=int)
+
+        clim = compute_climatology(
+            p_values, n_days, step_window, freq, stride, dates, dis
+        )
+
+        clim.to_netcdf(args.output)
---
+++ tools/crps-stations.py
@@ -0,0 +1,280 @@
+#!/usr/bin/env python3
+
+import argparse
+import glob
+import logging as log
+import os
+from os import path
+
+import dask
+import numpy as np
+import pandas as pd
+import xarray as xr
+from danu import stats, utils
+
+
+@dask.delayed
+def persistence_crps(reanalysis, persistence):
+
+    # compute statistics for one forecast date
+    crps_pers = np.fabs(reanalysis - persistence)
+
+    return crps_pers
+
+
+def shift_dates(dates, istart, n_dates=104, days=[0, 4]):
+
+    dt = dates[1] - dates[0]
+
+    if istart.dtype == np.int64:
+        print("startdate coordinates not provided, reconstructing...")
+        date_year = int(istart % n_dates)
+        year_shift = int(istart / n_dates)
+        week_shift = int(date_year / len(days))
+        day_shift = days[int(date_year % len(days))]
+
+        # compute forecast date
+        fc_date_ref = (
+            dates[0].values
+            + np.timedelta64(day_shift, "D")
+            + np.timedelta64(week_shift, "W")
+        )
+        date_pd = pd.to_datetime(fc_date_ref)
+        year = date_pd.year + year_shift
+        fc_date = np.datetime64(date_pd.replace(year=year))
+    else:
+        fc_date = istart + dt
+
+    new_dates = xr.DataArray(np.empty(len(dates), dtype=np.datetime64), dims=["time"])
+    for i in range(len(dates)):
+        new_dates[i] = fc_date + i * dt
+
+    return new_dates
+
+
+def coord_dmh(dates):
+    dates_str = np.datetime_as_string(dates, unit="h")
+    days_months = [date[5:] for date in dates_str]
+    return days_months
+
+
+@utils.timer
+def compute_score(
+    out_dir, reforecast_dir, ds_reanalysis, ds_clim, core_dims, with_init=False
+):
+
+    print("\nComputing crps and crpss\n")
+
+    reforecast_files = glob.glob(reforecast_dir + "/*.nc")
+    da_ref = xr.open_dataset(os.path.join(args.reforecast, reforecast_files[0]))
+    set1 = set(da_ref.station.values)
+    set2 = set(ds_reanalysis.station.values)
+    stations = list(set1.intersection(set2))
+    da_ref = da_ref.sel(station=stations)
+    da_ref = da_ref["dis"].sel(ensemble=0)
+
+    crps_refc_mean = xr.DataArray(np.zeros(da_ref.shape), dims=da_ref.dims)
+    crps_pers_mean = xr.DataArray(np.zeros(da_ref.shape), dims=da_ref.dims)
+    if ds_clim is not None:
+        crps_clim_mean = xr.DataArray(np.zeros(da_ref.shape), dims=da_ref.dims)
+
+    n_dates = len(reforecast_files)
+    log.info("Number of reforecast datasets in folder: " + str(n_dates))
+
+    count = 0
+    for reforecast_path in reforecast_files:
+
+        log.info("- {}: {}".format(count, reforecast_path))
+        ds_reforecast = xr.open_dataset(os.path.join(args.reforecast, reforecast_path))
+
+        set1 = set(ds_reforecast.station.values)
+        set2 = set(ds_reanalysis.station.values)
+        stations = list(set1.intersection(set2))
+        ds_reforecast = ds_reforecast.sel(station=stations)
+        ds_reanalysis = ds_reanalysis.sel(station=stations)
+
+        date_range = ds_reforecast.time
+        step = (date_range.values[1] - date_range.values[0]).astype(
+            "timedelta64[h]"
+        ) / np.timedelta64(1, "h")
+        date_persistence = date_range[0]
+        if not with_init:
+            date_persistence = date_persistence - np.timedelta64(int(step), "h")
+        log.info(
+            "First step: {}, time step: {} hours".format(date_range.values[0], step)
+        )
+        log.info("Persistence date is {}".format(date_persistence.values))
+
+        reforecast = ds_reforecast["dis"].sel(time=date_range)
+        reforecast = reforecast.transpose("time", "ensemble", *core_dims)
+
+        # extract arrays of interest
+        reanalysis = ds_reanalysis.sel(time=date_range)
+        persistence = ds_reanalysis.sel(time=date_persistence)
+
+        # num = 10
+        # print(reforecast.isel(station=num, time=1).values)
+        # print(reanalysis.isel(station=num, time=1))
+        # print(persistence.isel(station=num))
+        # compute statistics for one forecast date
+        # print(reanalysis.station.values)
+        # print(reforecast.station.values)
+        # for i in range(len(reanalysis.station.values)):
+        #     print(reanalysis.station.values[i], reforecast.station.values[i])
+        #     assert reforecast.station.values[i] == reanalysis.station.values[i]
+        crps_pers = persistence_crps(reanalysis, persistence)
+        crps_refo = stats.forecast_crps(reforecast, reanalysis, core_dims=core_dims)
+        if ds_clim is not None:
+            log.debug(coord_dmh(date_range))
+            climatology = ds_clim.sel(time=coord_dmh(date_range))
+            climatology.coords["time"] = date_range
+            crps_clim = stats.forecast_crps(
+                climatology, reanalysis, core_dims=core_dims
+            )
+            crps_refo, crps_pers, crps_clim = dask.compute(
+                crps_refo, crps_pers, crps_clim
+            )
+        else:
+            crps_refo, crps_pers = dask.compute(crps_refo, crps_pers)
+
+        # print(crps_refo.isel(station=num, time=1).values)
+        # print(crps_pers.isel(station=num, time=1).values)
+        # exit(1)
+
+        # write forecast files
+        if out_dir:
+            crps_refo = crps_refo.rename("crps")
+            refo_file = path.join(out_dir, "crps_refo_{:04}.nc".format(int(count)))
+            crps_refo.to_netcdf(refo_file)
+            crps_pers = crps_pers.rename("crps")
+            pers_file = path.join(out_dir, "crps_pers_{:04}.nc".format(int(count)))
+            crps_pers.to_netcdf(pers_file)
+            if ds_clim is not None:
+                crps_clim = crps_clim.rename("crps")
+                clim_file = path.join(out_dir, "crps_clim_{:04}.nc".format(int(count)))
+                crps_clim.to_netcdf(clim_file)
+
+        crps_refo = crps_refo.drop("time")
+        crps_pers = crps_pers.drop("time")
+        crps_refc_mean = crps_refc_mean + crps_refo
+        crps_pers_mean = crps_pers_mean + crps_pers
+        if ds_clim is not None:
+            crps_clim = crps_clim.drop("time")
+            crps_clim_mean = crps_clim_mean + crps_clim
+
+        count += 1
+
+    # write statistics files
+    crps_refc_mean = crps_refc_mean / n_dates
+    crps_pers_mean = crps_pers_mean / n_dates
+
+    print(crps_refc_mean.isel(station=range(10)))
+    crps_refc_mean = crps_refc_mean.rename("crps")
+    crps_refc_mean.to_netcdf("crps_refo.nc")
+    print(crps_pers_mean.isel(station=range(10)))
+    crps_pers_mean = crps_pers_mean.rename("crps")
+    crps_pers_mean.to_netcdf("crps_pers.nc")
+
+    crpss_pers = 1 - (crps_refc_mean / crps_pers_mean)
+    crpss_pers = crpss_pers.rename("crpss")
+    print(crpss_pers.isel(station=range(10)))
+    crpss_pers.to_netcdf("crpss_pers.nc")
+
+    if ds_clim is not None:
+        crps_clim_mean = crps_clim_mean / n_dates
+        print(crps_clim_mean.isel(station=range(10)))
+        crps_clim_mean = crps_clim_mean.rename("crps")
+        crps_clim_mean.to_netcdf("crps_clim.nc")
+
+        crpss_clim = 1 - (crps_refc_mean / crps_clim_mean)
+        crpss_clim = crpss_clim.rename("crpss")
+        print(crpss_clim.isel(station=range(10)))
+        crpss_clim.to_netcdf("crpss_clim.nc")
+
+    assert count == n_dates
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reanalysis", required=True, help="reanalysis dataset file")
+    parser.add_argument("--reforecast", required=True, help="reforecast dataset folder")
+    parser.add_argument("--climatology", help="reanalysis dataset file")
+    parser.add_argument("--output", help="output folder for individual crps values")
+    parser.add_argument("--core_dim", default="station", help="name of core dimension")
+    parser.add_argument(
+        "--with_init",
+        action="store_true",
+        help="Activate if reforecast dataset does not include initial condition",
+    )
+    parser.add_argument(
+        "--scheduler",
+        default="threads",
+        choices=["synchronous", "threads"],
+        help="reanalysis dataset file",
+    )
+    parser.add_argument(
+        "--log",
+        default="INFO",
+        choices=["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
+        help="set logging level",
+    )
+
+    args = parser.parse_args()
+    log.basicConfig(
+        level=args.log, format="crps - (%(processName)-10s) %(levelname)s: %(message)s"
+    )
+
+    log.info("Computing the scoring using crps approach")
+
+    with dask.config.set(scheduler="threads"):
+
+        core_dim = args.core_dim
+        core_dims = ["station"]
+
+        # read reanalysis dataset
+        ds_reanalysis = xr.open_dataset(args.reanalysis)
+
+        # read first reforecast dataset for check
+        reforecast_files = glob.glob(os.path.join(args.reforecast, "*.nc"))
+        print("Found {} files".format(len(reforecast_files)))
+        print(reforecast_files)
+        ds_reforecast = xr.open_dataset(reforecast_files[0])
+
+        ds_reforecast = ds_reforecast.rename({core_dim: "station"})
+        ds_reanalysis = ds_reanalysis.rename({core_dim: "station"})
+
+        print("Reanalysis dataset from {}:".format(args.reanalysis))
+        print(ds_reanalysis["dis"])
+        print("Reforecast dataset from {}:".format(args.reforecast))
+        print(ds_reforecast["dis"])
+
+        set1 = set(ds_reforecast.station.values)
+        set2 = set(ds_reanalysis.station.values)
+        stations = list(set1.intersection(set2))
+        ds_reforecast = ds_reforecast.sel(station=stations)
+        ds_reanalysis = ds_reanalysis.sel(station=stations)
+        print(ds_reanalysis["dis"])
+        print(ds_reforecast["dis"])
+
+        ds_clim = None
+        if args.climatology:
+            ds_clim = xr.open_dataarray(args.climatology, chunks={"time": 1})
+            if core_dim != "station":
+                ds_clim = ds_clim.rename({core_dim: "station"})
+            ds_clim = ds_clim.assign_coords(
+                {"station": ds_reanalysis.coords["station"]}
+            )
+            print(ds_clim)
+
+        if args.output:
+            os.makedirs(args.output, exist_ok=True)
+
+        compute_score(
+            args.output,
+            args.reforecast,
+            ds_reanalysis["dis"],
+            ds_clim,
+            core_dims,
+            args.with_init,
+        )
